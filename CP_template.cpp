2803d6629156f9c38ca51f07586109caef8455ee
Tran Hai Minh <minhchillinbtw@gmail.com>
2026-07-10 21:50:57 +0700

    Update CP_template.cpp

--- CP_template.cpp
+++ CP_template.cpp
@@ -1,20 +1,20 @@
 #include <bits/stdc++.h>
-#define ll long long
-#define ull unsigned long long
-#define fi first
-#define se second
-#define pb push_back
-#define endl '\n'
+#define ll                      long long
+#define ull                     unsigned long long
+#define fi                      first
+#define se                      second
+#define pb                      push_back
+#define endl                    '\n'
 
-#define getbit(x, k) ((x) >> (k) & 1)
-#define onbit(x, k) ((x)  (1 << (k)))
-#define offbit(x, k) ((x) & ~(1 << (k)))
-#define mod(x, k) ((((x) % (k)) + (k)) % (k))
-#define compress(x) sort((x).begin(), (x).end()); (x).erase(unique((x).begin(), (x).end()), (x).end());
-#define debug(x) cerr << #x <<  " = " << x << endl;
+#define getbit(x, k)            ((x) >> (k) & 1)
+#define onbit(x, k)             ((x) | (1 << (k)))
+#define offbit(x, k)            ((x) & ~(1 << (k)))
+#define mod(x, k)               ((((x) % (k)) + (k)) % (k))
+#define compress(x)             sort((x).begin(), (x).end()); (x).erase(unique((x).begin(), (x).end()), (x).end());
+#define debug(x)                cerr << #x <<  " = " << x << endl;
 
-#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-#define imissher(xquynh) if (fopen("test.INP", "r")) {freopen("test.INP", "r", stdin);} else if (fopen(xquynh".INP", "r")) {freopen(xquynh".INP", "r", stdin); freopen(xquynh".OUT", "w", stdout);}
+#define fastio                  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define imissher(xquynh)        if (fopen("test.INP", "r")) {freopen("test.INP", "r", stdin);} else if (fopen(xquynh".INP", "r")) {freopen(xquynh".INP", "r", stdin); freopen(xquynh".OUT", "w", stdout);}
 
 using namespace std;
 
@@ -22,11 +22,16 @@ const int LOG = 20;
 const ll INF = 1e18;
 const ll MOD = 1e9 + 7;
 
-template <class T> bool maximize(T &a, const T &b) {return (a < b ? (a = b, true) : false);}
-template <class T> bool minimize(T &a, const T &b) {return (a > b ? (a = b, true) : false);}
+template <class T> bool maximize(T &a, const T &b){
+        return (a < b ? (a = b, true) : false);
+}
+
+template <class T> bool minimize(T &a, const T &b){
+        return (a > b ? (a = b, true) : false);
+}
 
 void anhlamgixungdangvoiem(){
-
+        
 }
 
 int main(){
